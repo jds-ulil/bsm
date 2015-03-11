859ec8cf69a0d0632ce4d0c3176ce30b62d1504f
Ulil <ulil@jakartadenshi.com>
2015-03-11 16:47:15 +0700

    add db
    Signed-off-by: Ulil <ulil@jakartadenshi.com>

--- database_with_daily_added.sql
+++ database_with_daily_added.sql
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `daily_bo`;
 
 CREATE TABLE `daily_bo` (
   `daily_bo_id` int(10) NOT NULL AUTO_INCREMENT,
-  `jumlah_transaksi` int(3) DEFAULT NULL,
+  `jumlah_transaksi` int(10) DEFAULT NULL,
   `kriteria_transaksi` int(2) DEFAULT NULL,
   `total` float DEFAULT '0',
   `nama_pegawai` varchar(70) COLLATE utf8_bin DEFAULT NULL,
@@ -31,9 +31,11 @@ CREATE TABLE `daily_bo` (
   `status` int(1) DEFAULT '1',
   `status_transaksi` int(1) DEFAULT NULL,
   PRIMARY KEY (`daily_bo_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `daily_bo` */
+
+insert  into `daily_bo`(`daily_bo_id`,`jumlah_transaksi`,`kriteria_transaksi`,`total`,`nama_pegawai`,`info`,`tanggal`,`status`,`status_transaksi`) values (2,2222,1,1111,'Juned','221','0000-00-00',2,1),(3,121,12,121,'Juned','122','0000-00-00',1,1),(4,12211,5,0,'Juned','','0000-00-00',1,1),(5,124,3,0,'Saripin','22','2015-03-11',1,3),(6,21222222,3,2121,'Saripin','122','2015-03-11',1,1),(7,1221,7,1221,'Saripin','21122','2015-03-11',1,1),(8,111122,1,0,'resi','','2015-03-11',1,1),(9,2147483647,7,0,'resi','','2015-03-11',1,1),(10,2147483647,1,1111,'resi','12','2015-03-11',1,1);
 
 /*Table structure for table `daily_bo_kriteria_transaksi` */
 
@@ -77,11 +79,11 @@ CREATE TABLE `daily_cs` (
   `tanggal` date DEFAULT NULL,
   `status` int(1) DEFAULT '1',
   PRIMARY KEY (`daily_cs_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `daily_cs` */
 
-insert  into `daily_cs`(`daily_cs_id`,`kriteria_nasabah`,`jumlah`,`total`,`nama_pegawai`,`info`,`tanggal`,`status`) values (1,2,12,22,'Ulil','Nice','0000-00-00',1),(2,3,56,120000,'Ulil','goode','0000-00-00',1),(4,1,12,111,'Raisa','','2015-02-05',2),(7,3,32,5000000,'Jacka','122','2015-02-23',2);
+insert  into `daily_cs`(`daily_cs_id`,`kriteria_nasabah`,`jumlah`,`total`,`nama_pegawai`,`info`,`tanggal`,`status`) values (1,2,12,22,'Ulil','Nice','0000-00-00',1),(2,3,56,120000,'Ulil','goode','0000-00-00',1),(4,1,12,111,'Raisa','','2015-02-05',2),(7,3,32,5000000,'Jacka','122','2015-02-23',2),(8,1,0,0,'','','2015-03-10',1),(9,7,0,0,'','','2015-03-10',1),(10,5,0,0,'','','2015-03-10',1),(11,4,0,0,'','','2015-03-10',1);
 
 /*Table structure for table `daily_cs_kriteria_nasabah` */
 
@@ -97,6 +99,40 @@ CREATE TABLE `daily_cs_kriteria_nasabah` (
 
 insert  into `daily_cs_kriteria_nasabah`(`cs_kriteria_nasabah_id`,`nama`) values (1,'Tabungan BSM'),(2,'Tabungan Simpatik'),(3,'Tabungan Berencana'),(4,'Tabungan Investa Cendikia'),(5,'Tabungan Mabrur'),(6,'TabunganKu'),(7,'Giro'),(8,'Deposito'),(9,'Talangan Haji'),(10,'TabunganKu'),(11,'Giro'),(12,'BSM Mobile Banking'),(13,'BSM Net Banking'),(14,'Top-Up Nasabah Eksisting'),(15,'Follow-Up Rekening Dormant'),(16,'Follow-Up Past Due haji'),(17,'Waktu Istirahat'),(18,'SE yang dibaca & dipahami'),(19,'Lain - Lain');
 
+/*Table structure for table `daily_sa` */
+
+DROP TABLE IF EXISTS `daily_sa`;
+
+CREATE TABLE `daily_sa` (
+  `daily_sa_id` int(10) NOT NULL AUTO_INCREMENT,
+  `jumlah_nasabah` int(10) DEFAULT '0',
+  `no_kontak` varchar(25) COLLATE utf8_bin DEFAULT NULL,
+  `total` float DEFAULT NULL,
+  `segmen` varchar(25) COLLATE utf8_bin DEFAULT NULL,
+  `nama_pegawai` varchar(25) COLLATE utf8_bin DEFAULT NULL,
+  `info` text COLLATE utf8_bin,
+  `status` int(1) DEFAULT '1',
+  `tanggal` date DEFAULT NULL,
+  `kriteria_nasabah` int(2) DEFAULT NULL,
+  PRIMARY KEY (`daily_sa_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `daily_sa` */
+
+/*Table structure for table `daily_sa_kriteria_nasabah` */
+
+DROP TABLE IF EXISTS `daily_sa_kriteria_nasabah`;
+
+CREATE TABLE `daily_sa_kriteria_nasabah` (
+  `sa_kriteria_nasabah_id` int(2) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(70) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`sa_kriteria_nasabah_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `daily_sa_kriteria_nasabah` */
+
+insert  into `daily_sa_kriteria_nasabah`(`sa_kriteria_nasabah_id`,`nama`) values (1,'Lending - Sosialisasi'),(2,'Lending - Solisit'),(3,'Lending - Pengumpulan Data'),(4,'Lending - BI Checking'),(5,'Lending - Taksasi Agunan'),(6,'Lending - Investigasi'),(7,'Lending - Analisa'),(8,'Lending - SP3'),(9,'Lending - Akad'),(10,'Lending - Pencairan'),(11,'Perpanjangan - Pengumpulan Data'),(12,'Perpanjangan - BI Checking'),(13,'Perpanjangan - Taksasi Agunan'),(14,'Perpanjangan - Investigasi'),(15,'Perpanjangan - Analisa'),(16,'Perpanjangan - SP3'),(17,'Perpanjangan - Akad'),(18,'Perpanjangan - Eksekusi Perpanjangan'),(19,'Pick-up Angsuran Nasabah'),(20,'Tagih Past Due Bulan Sebelumnya'),(21,'Peringatan Nasabah - SPMK'),(22,'Peringatan Nasabah - SP1'),(23,'Peringatan Nasabah - SP2'),(24,'Peringatan Nasabah - SP3'),(25,'Funding - Sosialisasi'),(26,'Funding - Solisit'),(27,'Funding - Follow Up'),(28,'Funding - Closing'),(29,'Pick-up Tabungan Nasabah'),(30,'SE yang dibaca & dipahami'),(31,'Waktu Istirahat'),(32,'Lain - Lain'),(33,NULL);
+
 /*Table structure for table `daily_security` */
 
 DROP TABLE IF EXISTS `daily_security`;
@@ -110,11 +146,11 @@ CREATE TABLE `daily_security` (
   `info` varchar(100) COLLATE utf8_bin NOT NULL,
   `status` int(1) DEFAULT '1',
   PRIMARY KEY (`daily_security_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `daily_security` */
 
-insert  into `daily_security`(`daily_security_id`,`nama_inputer`,`tanggal`,`jenis_nasabah`,`jumlah`,`info`,`status`) values (1,'Jono','2015-01-22',1,12,'Nice',2),(2,'hld','2015-02-02',1,3,'1',2),(3,'21','2015-01-22',5,14,'',2),(4,'aku','2015-02-04',1,13,'221',2);
+insert  into `daily_security`(`daily_security_id`,`nama_inputer`,`tanggal`,`jenis_nasabah`,`jumlah`,`info`,`status`) values (1,'Jono','2015-01-22',1,12,'Nice',2),(2,'hld','2015-02-02',1,3,'1',2),(3,'21','2015-01-22',5,14,'',2),(4,'aku','2015-02-04',1,13,'221',2),(5,'','2015-03-10',5,0,'',1);
 
 /*Table structure for table `daily_security_jenis_nasabah` */
 
@@ -158,11 +194,11 @@ CREATE TABLE `daily_teller` (
   `tanggal` date DEFAULT NULL,
   `status` int(1) DEFAULT NULL,
   PRIMARY KEY (`daily_teller_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `daily_teller` */
 
-insert  into `daily_teller`(`daily_teller_id`,`nama_pegawai`,`kriteria_transaksi`,`jumlah`,`total`,`info`,`tanggal`,`status`) values (1,'None',1,12,345000,'','2015-02-24',1),(3,'None',8,30,2500000,'Sip','2015-02-24',2);
+insert  into `daily_teller`(`daily_teller_id`,`nama_pegawai`,`kriteria_transaksi`,`jumlah`,`total`,`info`,`tanggal`,`status`) values (1,'None',1,12,345000,'','2015-02-24',1),(3,'None',8,30,2500000,'Sip','2015-02-24',2),(4,'',12,0,0,'','2015-03-10',1),(5,'',2,0,0,'','2015-03-10',1),(6,'',2,0,0,'','2015-03-10',1);
 
 /*Table structure for table `daily_teller_kriteria_transaksi` */
 
@@ -177,6 +213,39 @@ CREATE TABLE `daily_teller_kriteria_transaksi` (
 /*Data for the table `daily_teller_kriteria_transaksi` */
 
 insert  into `daily_teller_kriteria_transaksi`(`jenis_transaksi_id`,`nama`) values (1,'Total Transaksi'),(2,'Transaksi Setoran'),(3,'Transaksi Penarikan'),(4,'Transaksi Net Banking'),(5,'Transaksi Transfer Tunai'),(6,'Saldo Teller Akhir Hari'),(7,'Saldo Khasanah Akhir Hari'),(8,'Saldo ATM Akhir Hari'),(9,'Cross Selling'),(10,'Waktu Istirahat'),(11,'SE yang dibaca & dipahami'),(12,'Lain - Lain');
+
+/*Table structure for table `daily_wm` */
+
+DROP TABLE IF EXISTS `daily_wm`;
+
+CREATE TABLE `daily_wm` (
+  `daily_wm_id` int(10) NOT NULL AUTO_INCREMENT,
+  `jumlah_nasabah` int(10) DEFAULT '0',
+  `kriteria_nasabah` int(2) DEFAULT NULL,
+  `no_kontak` varchar(25) COLLATE utf8_bin DEFAULT NULL,
+  `total` float DEFAULT NULL,
+  `nama_pegawai` varchar(70) COLLATE utf8_bin DEFAULT NULL,
+  `info` text COLLATE utf8_bin,
+  `status` int(1) DEFAULT '1',
+  `tanggal` date DEFAULT NULL,
+  PRIMARY KEY (`daily_wm_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `daily_wm` */
+
+/*Table structure for table `daily_wm_kriteria_nasabah` */
+
+DROP TABLE IF EXISTS `daily_wm_kriteria_nasabah`;
+
+CREATE TABLE `daily_wm_kriteria_nasabah` (
+  `wm_kriteria_nasabah_id` int(2) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(70) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`wm_kriteria_nasabah_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `daily_wm_kriteria_nasabah` */
+
+insert  into `daily_wm_kriteria_nasabah`(`wm_kriteria_nasabah_id`,`nama`) values (1,'Tahap - Sosialisasi'),(2,'Tahap - Solisit'),(3,'Tahap - Pengumpulan Data'),(4,'Tahap - BI Checking'),(5,'Tahap - Taksasi Agunan'),(6,'Tahap - Investigasi'),(7,'Tahap - Analisa'),(8,'Tahap - SP3'),(9,'Tahap - Akad'),(10,'Tahap - Pencairan'),(11,'Pick-up Angsuran Nasabah'),(12,'Tagih Past Due Bulan Sebelumnya'),(13,'Peringatan Nasabah - SPMK'),(14,'Peringatan Nasabah - SP1'),(15,'Peringatan Nasabah - SP2'),(16,'Peringatan Nasabah - SP3'),(17,'Funding - Sosialisasi'),(18,'Funding - Solisit'),(19,'Funding - Follow Up'),(20,'Funding - Closing'),(21,'Pick-up Tabungan Nasabah'),(22,'SE yang dibaca & dipahami'),(23,'Waktu Istirahat'),(24,'Lain - Lain');
 
 /*Table structure for table `error_nasabah` */
 
